3a0083e16b7686c5539624e2128029ea10489783
Latisha Pickard <latisha0017@gmail.com>
2022-03-18 05:37:52 +0000

    changed gitignore file

--- MyGP/wsgi.py
+++ MyGP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for doctors project.
+WSGI config for MyGP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doctors.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyGP.settings')
 
 application = get_wsgi_application()
